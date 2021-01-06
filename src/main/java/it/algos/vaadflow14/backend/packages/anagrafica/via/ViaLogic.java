d15b45a49949669ee5ae41ad4f844481e16720d5
algos-soft <gac@algos.it>
2021-01-06 15:52:55 +0100

    ...

--- src/main/java/it/algos/vaadflow14/backend/packages/anagrafica/via/ViaLogic.java
+++ src/main/java/it/algos/vaadflow14/backend/packages/anagrafica/via/ViaLogic.java
@@ -67,7 +67,6 @@ public class ViaLogic extends ALogic {
     @Override
     protected void fixPreferenze() {
         super.fixPreferenze();
-
         super.usaBottoneDelete = true;
         super.usaBottoneResetList = true;
     }
