962aaef4f773c021d70e82297b34a8d07e96c2ca
algos-soft <gac@algos.it>
2021-02-16 18:27:13 +0100

    ...

---
+++ src/main/java/it/algos/vaadflow14/ui/header/AHeaderSpan.java
@@ -0,0 +1,51 @@
+package it.algos.vaadflow14.ui.header;
+
+import com.vaadin.flow.component.*;
+import com.vaadin.flow.component.html.*;
+import com.vaadin.flow.component.orderedlayout.*;
+import com.vaadin.flow.spring.annotation.*;
+import org.springframework.beans.factory.config.*;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.*;
+import java.util.*;
+
+/**
+ * Project vaadflow14
+ * Created by Algos
+ * User: gac
+ * Date: mar, 16-feb-2021
+ * Time: 11:30
+ */
+@SpringComponent
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+public class AHeaderSpan extends VerticalLayout implements AIHeader {
+
+    private List<Span> spanList;
+
+    /**
+     * Costruttore base con parametro <br>
+     * Non usa @Autowired perché l' istanza viene creata con appContext.getBean(AHeaderSpan.class, spanList) <br>
+     */
+    public AHeaderSpan(List<Span> spanList) {
+        this.setMargin(false);
+        this.setSpacing(false);
+        this.setPadding(false);
+        this.spanList = spanList;
+    }
+
+    @PostConstruct
+    private void postConstruct() {
+        if (spanList != null && spanList.size() > 0) {
+            for (Span span : spanList) {
+                this.add(span);
+            }
+        }
+    }
+
+    @Override
+    public Component get() {
+        return this;
+    }
+
+}
