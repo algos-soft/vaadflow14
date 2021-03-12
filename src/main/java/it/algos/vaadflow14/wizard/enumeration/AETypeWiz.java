361d7114c0384b4feac425bf09b954a98835a506
algos-soft <gac@algos.it>
2021-03-12 06:59:25 +0100

    ...

---
+++ src/main/java/it/algos/vaadflow14/wizard/enumeration/AETypeWiz.java
@@ -0,0 +1,12 @@
+package it.algos.vaadflow14.wizard.enumeration;
+
+/**
+ * Project wikibio
+ * Created by Algos
+ * User: gac
+ * Date: mer, 10-mar-2021
+ * Time: 16:07
+ */
+public enum AETypeWiz {
+    sistema, directory, nomeFile,path, packages
+}
---
+++ src/main/java/it/algos/vaadflow14/wizard/scripts/WizDialogPackage.java
@@ -0,0 +1,27 @@
+package it.algos.vaadflow14.wizard.scripts;
+
+import it.algos.vaadflow14.wizard.enumeration.*;
+
+/**
+ * Project wikibio
+ * Created by Algos
+ * User: gac
+ * Date: gio, 11-mar-2021
+ * Time: 21:19
+ */
+public abstract class WizDialogPackage extends WizDialog {
+
+    /**
+     * Chiamato alla dismissione del dialogo <br>
+     * Regola i valori regolabili della Enumeration AEWizCost <br>
+     * Ci sono diversi VALORE_MANCANTE di cui 7 regolati all'ingresso del dialogo <br>
+     */
+    protected boolean regolaPackages(final String packName) {
+        wizService.regolaPackages(packName);
+
+        AEWizCost.printVuote();
+        AEWizCost.printInfoBase(AEWizCost.getPackages(), "Variabili del package. Dipende dal package selezionato");
+        return true;
+    }
+
+}
