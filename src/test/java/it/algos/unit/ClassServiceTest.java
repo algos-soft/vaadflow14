095a8a8d3732602a99dd8549ab633ef6153a7adb
algos-soft <gac@algos.it>
2021-05-16 18:04:10 +0200

    ...

---
+++ src/test/java/it/algos/unit/ClassServiceTest.java
@@ -0,0 +1,121 @@
+package it.algos.unit;
+
+import it.algos.test.*;
+import static it.algos.vaadflow14.backend.application.FlowCost.*;
+import it.algos.vaadflow14.backend.service.*;
+import static org.junit.Assert.*;
+import org.junit.jupiter.api.*;
+import org.mockito.*;
+
+/**
+ * Project vaadflow14
+ * Created by Algos
+ * User: gac
+ * Date: dom, 16-mag-2021
+ * Time: 17:41
+ * Unit test di una classe di servizio <br>
+ * Estende la classe astratta ATest che contiene le regolazioni essenziali <br>
+ * Nella superclasse ATest vengono iniettate (@InjectMocks) tutte le altre classi di service <br>
+ * Nella superclasse ATest vengono regolati tutti i link incrociati tra le varie classi classi singleton di service <br>
+ */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@Tag("ClassServiceTest")
+@DisplayName("Test di unit")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class ClassServiceTest extends ATest {
+
+
+    /**
+     * Classe principale di riferimento <br>
+     */
+    @InjectMocks
+    AClassService service;
+
+
+    /**
+     * Qui passa una volta sola, chiamato dalle sottoclassi <br>
+     * Invocare PRIMA il metodo setUpStartUp() della superclasse <br>
+     * Si possono aggiungere regolazioni specifiche <br>
+     */
+    @BeforeAll
+    void setUpAll() {
+        super.setUpStartUp();
+
+        MockitoAnnotations.initMocks(this);
+        MockitoAnnotations.initMocks(service);
+        Assertions.assertNotNull(service);
+        service.text = text;
+        service.array = array;
+    }
+
+
+    /**
+     * Qui passa ad ogni test delle sottoclassi <br>
+     * Invocare PRIMA il metodo setUp() della superclasse <br>
+     * Si possono aggiungere regolazioni specifiche <br>
+     */
+    @BeforeEach
+    void setUpEach() {
+        super.setUp();
+    }
+
+    @Test
+    @Order(1)
+    @DisplayName("1 - Test 'a freddo' (senza service)")
+    void first() {
+        Class clazz = VIA_ENTITY_CLASS;
+        String canonicalName = clazz.getCanonicalName();
+        assertTrue(text.isValid(canonicalName));
+        System.out.println(canonicalName);
+        Class clazz2 = null;
+        Class clazz3 = null;
+
+        try {
+            clazz2 = Class.forName(canonicalName);
+        } catch (Exception unErrore) {
+            logger.error(unErrore, this.getClass(), "nomeDelMetodo");
+        }
+        assertNotNull(clazz2);
+        System.out.println(clazz2.getSimpleName());
+        System.out.println(clazz2.getName());
+        System.out.println(clazz2.getCanonicalName());
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("2 - getClazzFromName")
+    void getClazzFromName() {
+        Class clazz;
+        sorgente = VIA_ENTITY_CLASS.getCanonicalName();
+
+        clazz = service.getClazzFromName(sorgente);
+        assertNotNull(clazz);
+        System.out.println(clazz.getSimpleName());
+        System.out.println(clazz.getName());
+        System.out.println(clazz.getCanonicalName());
+
+        sorgente2 = sorgente + JAVA_SUFFIX;
+        clazz = service.getClazzFromName(sorgente2);
+        assertNotNull(clazz);
+        System.out.println(clazz.getSimpleName());
+        System.out.println(clazz.getName());
+        System.out.println(clazz.getCanonicalName());
+
+    }
+
+    /**
+     * Qui passa al termine di ogni singolo test <br>
+     */
+    @AfterEach
+    void tearDown() {
+    }
+
+
+    /**
+     * Qui passa una volta sola, chiamato alla fine di tutti i tests <br>
+     */
+    @AfterEach
+    void tearDownAll() {
+    }
+
+}
