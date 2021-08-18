d07787b0451aaf0d77c976c41b4ca1e4ba244256
algos-soft <gac@algos.it>
2021-08-18 07:59:41 +0200

    ...

---
+++ src/test/java/it/algos/unit/GsonServiceTest.java
@@ -0,0 +1,178 @@
+package it.algos.unit;
+
+import it.algos.test.*;
+import it.algos.vaadflow14.backend.packages.anagrafica.via.*;
+import it.algos.vaadflow14.backend.packages.crono.giorno.*;
+import it.algos.vaadflow14.backend.service.*;
+import static org.junit.Assert.*;
+import org.junit.jupiter.api.*;
+import org.mockito.*;
+
+/**
+ * Project vaadflow14
+ * Created by Algos
+ * User: gac
+ * Date: mar, 17-ago-2021
+ * Time: 19:49
+ * Unit test di una classe di servizio <br>
+ * Estende la classe astratta ATest che contiene le regolazioni essenziali <br>
+ * Nella superclasse ATest vengono iniettate (@InjectMocks) tutte le altre classi di service <br>
+ * Nella superclasse ATest vengono regolati tutti i link incrociati tra le varie classi classi singleton di service <br>
+ */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@Tag("testAllValido")
+@DisplayName("Test Gson")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class GsonServiceTest extends ATest {
+
+    private static final String DATA_BASE_NAME = "vaadflow14";
+
+    /**
+     * Classe principale di riferimento <br>
+     */
+    @InjectMocks
+    GsonService service;
+
+
+    /**
+     * Qui passa una volta sola, chiamato dalle sottoclassi <br>
+     * Invocare PRIMA il metodo setUpStartUp() della superclasse <br>
+     * Si possono aggiungere regolazioni specifiche <br>
+     */
+    @BeforeAll
+    void setUpAll() {
+        super.setUpStartUp();
+
+        MockitoAnnotations.initMocks(this);
+        MockitoAnnotations.initMocks(service);
+        Assertions.assertNotNull(service);
+        service.text = text;
+        service.array = array;
+
+        service.fixProperties(DATA_BASE_NAME);
+    }
+
+
+    /**
+     * Qui passa ad ogni test delle sottoclassi <br>
+     * Invocare PRIMA il metodo setUp() della superclasse <br>
+     * Si possono aggiungere regolazioni specifiche <br>
+     */
+    @BeforeEach
+    void setUpEach() {
+        super.setUp();
+    }
+
+
+    @Test
+    @Order(1)
+    @DisplayName("1 - countGraffe")
+    void countGraffe() {
+        sorgente = "Forse domani ]] me ne vado [[ davvero [[ e forse";
+        previstoIntero = 0;
+        ottenutoIntero = service.countGraffe(sorgente);
+        assertEquals(previstoIntero, ottenutoIntero);
+
+        sorgente = "Forse domani ]] me ne vado { davvero [[ e forse { no { mercoledì";
+        previstoIntero = -1;
+        ottenutoIntero = service.countGraffe(sorgente);
+        assertEquals(previstoIntero, ottenutoIntero);
+
+        sorgente = "Forse domani ]] me ne [ vado { davvero [[ e [forse } no } mercoledì}}";
+        previstoIntero = -1;
+        ottenutoIntero = service.countGraffe(sorgente);
+        assertEquals(previstoIntero, ottenutoIntero);
+
+        sorgente = "Forse {domani ]] me ne [ vado davvero [[ e [forse no } mercoledì";
+        previstoIntero = 1;
+        ottenutoIntero = service.countGraffe(sorgente);
+        assertEquals(previstoIntero, ottenutoIntero);
+
+        sorgente = "Forse {domani ]] me ne [ vado { davvero [[ e [forse } no } mercoledì";
+        previstoIntero = 2;
+        ottenutoIntero = service.countGraffe(sorgente);
+        assertEquals(previstoIntero, ottenutoIntero);
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("2 - estraeGraffa")
+    void estraeGraffa() {
+        sorgente = "id\":\"5gennaio\",\"ordine\":5,\"giorno\":\"5 gennaio\",\"mese\":{\"id\":\"gennaio\",\"collectionName\":\"mese\"},\"reset\":true,\"class\":\"giorno";
+        previsto = "\"mese\":{\"id\":\"gennaio\",\"collectionName\":\"mese\"},";
+        ottenuto = service.estraeGraffa(sorgente);
+        assertEquals(previsto, ottenuto);
+    }
+
+
+    @Test
+    @Order(3)
+    @DisplayName("3 - eliminaGraffa")
+    void eliminaGraffa() {
+        sorgente = "id\":\"5gennaio\",\"ordine\":5,\"giorno\":\"5 gennaio\",\"mese\":{\"id\":\"gennaio\",\"collectionName\":\"mese\"},\"reset\":true,\"class\":\"giorno";
+        previsto = "id\":\"5gennaio\",\"ordine\":5,\"giorno\":\"5 gennaio\",\"reset\":true,\"class\":\"giorno";
+        ottenuto = service.eliminaGraffa(sorgente);
+        assertEquals(previsto, ottenuto);
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("4 - estraeGraffe (nessuna)")
+    void estraeGraffe() {
+        System.out.println("2 - Esistenza delle collezioni");
+
+        sorgente = "id\":\"5gennaio\",\"ordine\":5,\"giorno\":\"5 gennaio\",\"mese\":{\"id\":\"gennaio\",\"collectionName\":\"mese\"},\"reset\":true,\"class\":\"giorno";
+        previsto = "\"mese\":{\"id\":\"gennaio\",\"collectionName\":\"mese\"},";
+        ottenutoArray = service.estraeGraffe(sorgente);
+        assertEquals(previsto, ottenuto);
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("5 - estraeGraffe (una)")
+    void estraeGraffe1() {
+        sorgente = "id\":\"5gennaio\",\"ordine\":5,\"giorno\":\"5 gennaio\",\"mese\":{\"id\":\"gennaio\",\"collectionName\":\"mese\"},\"reset\":true,\"class\":\"giorno";
+        previsto = "\"mese\":{\"id\":\"gennaio\",\"collectionName\":\"mese\"},";
+        ottenutoArray = service.estraeGraffe(sorgente);
+        assertEquals(previsto, ottenuto);
+    }
+
+    @Test
+    @Order(6)
+    @DisplayName("6 - estraeGraffe (due)")
+    void estraeGraffe2() {
+        sorgente = "id\":\"5gennaio\",\"ordine\":5,\"giorno\":\"5 gennaio\",\"mese\":{\"id\":\"gennaio\",\"collectionName\":\"mese\"},\"reset\":true,\"class\":\"giorno";
+        previsto = "\"mese\":{\"id\":\"gennaio\",\"collectionName\":\"mese\"},";
+        ottenutoArray = service.estraeGraffe(sorgente);
+        assertEquals(previsto, ottenuto);
+    }
+
+    //    @Test
+    @Order(8)
+    @DisplayName("crea")
+    void crea() {
+        sorgente = "piazza";
+        entityBean = service.crea(Via.class, sorgente);
+        assertNotNull(entityBean);
+
+        sorgente = "5gennaio";
+        entityBean = service.crea(Giorno.class, sorgente);
+        assertNotNull(entityBean);
+    }
+
+    /**
+     * Qui passa al termine di ogni singolo test <br>
+     */
+    @AfterEach
+    void tearDown() {
+    }
+
+
+    /**
+     * Qui passa una volta sola, chiamato alla fine di tutti i tests <br>
+     */
+    @AfterEach
+    void tearDownAll() {
+    }
+
+}
